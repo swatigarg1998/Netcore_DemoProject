80c54a3e61b3649698e15eb117ddd9e3fdbd3607
gargs <gargs@AD0315-NN7LFCM>
2022-09-27 10:13:14 +0530

    test

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+
+        public static void Main(string[] a)
+        {
+            calculator obj = new calculator();
+ 
+
+        }
+    }
+
+
+  public class calculator
+    {
+        public int add (int a ,int b)
+        {
+            return a + b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int division(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+
+
+    // example of extension method 
+    static class ExtMehtodClass
+    {
+        public static int  Mod(this calculator calculator ,int a,int b )
+        {
+            return (a % b);
+        }
+    }
+}
